0643eda4524c0cee8da5de371e92c683fd2f363c
ToniMarkulj <markulj@gmail.com>
2019-12-08 02:21:24 -0800

    changed DB username and password

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :blog, Blog.Repo,
-  username: "hakatonimarukuro",
-  password: "tonideni1",
+  username: "postgres",
+  password: "postgres",
   database: "blog_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
